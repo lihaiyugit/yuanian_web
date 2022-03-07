5dbfe6effdce9d5a944fbbfbf1eb35d5413e1d90
kouxh <2890727230@qq.com>
2022-03-07 14:47:46 +0800

    策略、指挥、决策页面搭建

---
+++ public_js/page_aos.js
@@ -0,0 +1,6 @@
+AOS.init({
+    delay: 0, // values from 0 to 3000, with step 50ms
+    duration: 400, // values from 0 to 3000, with step 50ms
+    easing: 'ease', // default easing for AOS animations
+    once: true,
+});
